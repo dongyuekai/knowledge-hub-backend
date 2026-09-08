4cc69ddead6ae53302595b240750aa3d4b20018a
dongyuekai <457545648@qq.com>
2026-09-08 18:46:07 +0800

    feat: 用户鉴权模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,13 @@ import { AppService } from './app.service';
 import { DocumentModule } from './document/document.module';
 import { DocumentEntity } from './document/entities/document.entity';
 import { DocumentReviewEntity } from './document/entities/document-review.entity';
+import { AuthModule } from './auth/auth.module';
 import { MqModule } from './mq/mq.module';
 import { PipelineModule } from './pipeline/pipeline.module';
 import { StorageModule } from './storage/storage.module';
+import { UserEntity } from './user/entities/user.entity';
+import { RoleEntity } from './user/entities/role.entity';
+import { UserRoleEntity } from './user/entities/user-role.entity';
 
 @Module({
   imports: [
@@ -30,7 +34,13 @@ import { StorageModule } from './storage/storage.module';
         username: config.get<string>('POSTGRES_USER', 'user'),
         password: config.get<string>('POSTGRES_PASSWORD', '123456'),
         database: config.get<string>('POSTGRES_DB', 'knowledge_hub'),
-        entities: [DocumentEntity, DocumentReviewEntity],
+        entities: [
+          DocumentEntity,
+          DocumentReviewEntity,
+          UserEntity,
+          RoleEntity,
+          UserRoleEntity,
+        ],
         synchronize: false,
       }),
     }),
@@ -44,6 +54,7 @@ import { StorageModule } from './storage/storage.module';
       }),
     }),
     DocumentModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
